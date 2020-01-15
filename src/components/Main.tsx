4a9cf4f4ed5ab03006d66946e7ab77d4a26f71da
Ubuntu <ubuntu@ip-172-31-43-201.us-west-2.compute.internal>
2020-01-15 22:44:56 +0000

    Fixing README

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,18 +1,25 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
-import "./../assets/scss/Main.scss";
+import Layout from './Layout';
+
+const logo = require("./../assets/img/react_logo.svg");
+import "./../assets/scss/main.scss";
 
 class Main extends React.Component<{}, undefined> {
+    
     public render(): JSX.Element {
         return (
             <div className="main">
                 <h1>Hello World!</h1>
-                <img src={reactLogo.default} height="480"/>
+                <img src={logo.default} height="480"/>
+                <Layout>
+                    Goodbye, world.
+                </Layout>
             </div>
         );
     }
+    
 }
 
 export default hot(module)(Main);
